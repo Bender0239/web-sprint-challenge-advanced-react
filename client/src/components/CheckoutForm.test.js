f25d3d3e30f869aff0163b45ebb1042d0bda0bff
Bender0239 <hayden.bender@protonmail.com>
2020-08-07 09:21:28 -0700

    done with testing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+    const formHeader = screen.findByText(/checkout form/i)
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    const submitButton = screen.getByRole('button')
+    fireEvent.click(submitButton)
+    const successMessage = screen.getAllByTestId('successMessage')
+});
